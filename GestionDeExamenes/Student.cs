73dcaf572bde4e09cbbbc1855a7e91fcabdfae6a
JCamiloAgu <jcagudelo42@misena.edu.co>
2019-11-28 13:47:54 -0500

    Se inicializa el proyecto y se crea la clase Student

---
+++ GestionDeExamenes/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeExamenes
+{
+    class Student
+    {
+        private int regNumber { get; set; }
+        private string name { get; set; }
+        private int group { get; set; }
+        
+    }
+}
